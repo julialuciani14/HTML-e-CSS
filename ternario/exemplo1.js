302da9be080c1b9b233fc28680eddffe97979d79
Juju <lucianistefanojulia@gmail.com>
2026-09-11 13:11:07 -0300

    Add files via upload

---
+++ ternario/exemplo1.js
@@ -0,0 +1,11 @@
+let idade = 18
+
+if(idade >= 18){
+    console.log("Maior de idade")
+}else{
+    console.log("Menor de idade")
+}
+
+//condição ? valor for verdadeiro : valor se falso
+
+idade >= 18 ? console.log ("Maior de idade") : console.log ("Menor de idade")
